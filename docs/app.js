19d750940c93dce6dc08853b3b7fa20a89da2f3b
西村健介 <k.nishimura@MacBookPro.local>
2026-03-21 05:42:12 +0900

    Move Batist icon position further upward

--- docs/app.js
+++ docs/app.js
@@ -29,8 +29,8 @@ const RENDER_BATCH_SIZE = 200;
 const SUPABASE_TABLE = "lineup_states";
 const FIXED_SUPABASE_URL = "https://trbuptnlpmcetwprirxn.supabase.co";
 const FIXED_SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRyYnVwdG5scG1jZXR3cHJpcnhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5Nzg5MzIsImV4cCI6MjA4ODU1NDkzMn0.mPzL3tfKfWsCh17om16OGKYiayAhrhn3Cy74DXKGwI0";
-const APP_UPDATED_AT_ISO = "2026-03-21T05:40:21+09:00";
-const APP_UPDATED_AT_JST = "2026-03-21 05:40 JST";
+const APP_UPDATED_AT_ISO = "2026-03-21T05:41:54+09:00";
+const APP_UPDATED_AT_JST = "2026-03-21 05:41 JST";
 let appUpdatedAtJst = APP_UPDATED_AT_JST;
 
 function formatIsoToJstLabel(isoString) {
