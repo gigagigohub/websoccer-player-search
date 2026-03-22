e07ddf72aa2ea0adba936580f642a7e3649ab4aa
西村健介 <k.nishimura@MacBookPro.local>
2026-03-22 23:36:42 +0900

    Make page background scroll with content to remove fixed top band

--- docs/app.js
+++ docs/app.js
@@ -29,8 +29,8 @@ const RENDER_BATCH_SIZE = 200;
 const SUPABASE_TABLE = "lineup_states";
 const FIXED_SUPABASE_URL = "https://trbuptnlpmcetwprirxn.supabase.co";
 const FIXED_SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRyYnVwdG5scG1jZXR3cHJpcnhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5Nzg5MzIsImV4cCI6MjA4ODU1NDkzMn0.mPzL3tfKfWsCh17om16OGKYiayAhrhn3Cy74DXKGwI0";
-const APP_UPDATED_AT_ISO = "2026-03-22T23:30:32+09:00";
-const APP_UPDATED_AT_JST = "2026-03-22 23:30 JST";
+const APP_UPDATED_AT_ISO = "2026-03-22T23:36:33+09:00";
+const APP_UPDATED_AT_JST = "2026-03-22 23:36 JST";
 let appUpdatedAtJst = APP_UPDATED_AT_JST;
 
 function metricLabel(metric) {
