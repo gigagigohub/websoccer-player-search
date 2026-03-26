81974835e16387a4d14b8c03c223a0b89c5c3df3
西村健介 <k.nishimura@MacBookPro.local>
2026-03-26 20:32:58 +0900

    Adjust MyTeam formation plot offset and box spacing

--- docs/coaches.js
+++ docs/coaches.js
@@ -2,7 +2,7 @@ const CLOUD_CONFIG_STORAGE_KEY = "ws_cloud_config_v1";
 const SUPABASE_TABLE = "lineup_states";
 const FIXED_SUPABASE_URL = "https://trbuptnlpmcetwprirxn.supabase.co";
 const FIXED_SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRyYnVwdG5scG1jZXR3cHJpcnhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5Nzg5MzIsImV4cCI6MjA4ODU1NDkzMn0.mPzL3tfKfWsCh17om16OGKYiayAhrhn3Cy74DXKGwI0";
-const APP_UPDATED_AT_JST = "2026-03-26 20:29 JST";
+const APP_UPDATED_AT_JST = "2026-03-26 20:32 JST";
 
 const TYPE_LABELS = {
   1: "超攻撃型",
