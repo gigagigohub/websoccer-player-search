0c0831d0307a952d5df995353679764e08e00ac8
西村健介 <k.nishimura@MacBookPro.local>
2026-03-26 21:53:53 +0900

    Left-align MyTeam parameter numeric values

--- docs/app.js
+++ docs/app.js
@@ -30,8 +30,8 @@ const RENDER_CARDS_PER_FRAME = 24;
 const SUPABASE_TABLE = "lineup_states";
 const FIXED_SUPABASE_URL = "https://trbuptnlpmcetwprirxn.supabase.co";
 const FIXED_SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRyYnVwdG5scG1jZXR3cHJpcnhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5Nzg5MzIsImV4cCI6MjA4ODU1NDkzMn0.mPzL3tfKfWsCh17om16OGKYiayAhrhn3Cy74DXKGwI0";
-const APP_UPDATED_AT_ISO = "2026-03-26T21:43:00+09:00";
-const APP_UPDATED_AT_JST = "2026-03-26 21:43 JST";
+const APP_UPDATED_AT_ISO = "2026-03-26T21:53:00+09:00";
+const APP_UPDATED_AT_JST = "2026-03-26 21:53 JST";
 let appUpdatedAtJst = APP_UPDATED_AT_JST;
 
 function metricLabel(metric) {
